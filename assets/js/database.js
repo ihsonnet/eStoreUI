9c7df12576f792116a317be4543d9463e7e16ff5
Injamamul Haque Sonet <48057930+ihsonnet@users.noreply.github.com>
2020-07-15 19:47:02 +0600

    DB controller JS added

---
+++ assets/js/database.js
@@ -0,0 +1,18 @@
+
+function saveData() {
+  fname = document.getElementById("fname").value;
+  localStorage.setItem("fname",fname);
+  return false;
+}
+// let userInfo = [];
+// const saveData =
+
+
+
+function checkData() {
+
+}
+
+function saveStoreData() {
+
+}
