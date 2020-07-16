b3c211799fe0a28aa2a68fa30e46701f53dad1e5
Injamamul Haque Sonet <48057930+ihsonnet@users.noreply.github.com>
2020-07-16 15:17:31 +0600

    logreg process with localStorage

--- assets/js/database.js
+++ assets/js/database.js
@@ -1,18 +1,47 @@
-
 function saveData() {
+  let fname,lname;
+  let regEmail,regPass;
   fname = document.getElementById("fname").value;
-  localStorage.setItem("fname",fname);
-  return false;
+  lname = document.getElementById("lname").value;
+  regEmail= document.getElementById("regEmail").value;
+  regPass = document.getElementById("regPass").value;
+
+
+  localStorage.setItem("fName",fname);
+  localStorage.setItem("lName",lname);
+  localStorage.setItem("regEmail",regEmail);
+  localStorage.setItem("regPass",regPass);
+  return;
 }
-// let userInfo = [];
-// const saveData =
 
-
+function confirmation() {
+  window.location.replace("https://google.com");
+}
 
 function checkData() {
+  const  logEmail= document.getElementById("logEmail").value;
+  const  logPass = document.getElementById("logPass").value;
 
+  localStorage.setItem("logEmail",logEmail);
+  localStorage.setItem("logPass",logPass);
+
+  const myEmail = localStorage.getItem("regEmail");
+  const myPass = localStorage.getItem("regPass");
+  if(logEmail==myEmail && logPass==myPass)
+  {
+    window.location.replace("confirmation.html");
+    alert("Logged In");
+  }
+  else {
+    alert("Wrong Email or Pass.");
+  }
 }
 
-function saveStoreData() {
-
+function logOut(){
+  window.location.replace("confirmation.html");
+  alert("logged Out");
 }
+//
+// function saveStoreData() {
+//
+// }
