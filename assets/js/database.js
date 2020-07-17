0a03b71dd40fa17bbb9af63648f6aadb26f73cca
Injamamul Haque Sonet <48057930+ihsonnet@users.noreply.github.com>
2020-07-18 00:49:21 +0600

    update js store+dashboard+forgotPass

--- assets/js/database.js
+++ assets/js/database.js
@@ -14,9 +14,43 @@ function saveData() {
   return;
 }
 
+function storeData() {
+  let storeName,storeDes;
+  storeName = document.getElementById("storeName").value;
+  storeDes = document.getElementById("storeDes").value;
+
+  localStorage.setItem("storeName",storeName);
+  localStorage.setItem("storeDes",storeDes);
+  return;
+}
+function storeInfo() {
+  let storeName,storeDes;
+  storeCat = document.getElementById("storeCat").value;
+  storeAdress = document.getElementById("storeAddress").value;
+
+  localStorage.setItem("storeCat",storeCat);
+  localStorage.setItem("storeAddress",storeAddress);
+  return;
+}
+
 function confirmation() {
   window.location.replace("https://google.com");
 }
+
+function forgotPass(){
+  const  forEmail= document.getElementById("forEmail").value;
+  const myEmail = localStorage.getItem("regEmail");
+  if(forEmail==myEmail)
+  {
+    document.getElementById('seePass').style.display='block';
+    document.getElementById('errorEmail').style.display='none';
+  }
+  else {
+    document.getElementById('errorEmail').style.display='block';
+    document.getElementById('seePass').style.display='none';
+  }
+}
+
 
 function checkData() {
   const  logEmail= document.getElementById("logEmail").value;
@@ -27,21 +61,48 @@ function checkData() {
 
   const myEmail = localStorage.getItem("regEmail");
   const myPass = localStorage.getItem("regPass");
+  document.getElementById('errorEmail').style.display='none';
+  document.getElementById('errorPass').style.display='none';
   if(logEmail==myEmail && logPass==myPass)
   {
-    window.location.replace("confirmation.html");
-    alert("Logged In");
+    window.location.replace("dashboard.html");
+
+  }
+  else if (logPass==myPass) {
+    document.getElementById('errorEmail').style.display='block';
+  }
+  else if (logEmail==myEmail) {
+    document.getElementById('errorPass').style.display='block';
   }
   else {
-    alert("Wrong Email or Pass.");
+    alert("User not exist! Please Registration first.");
   }
 }
 
 function logOut(){
-  window.location.replace("confirmation.html");
+  window.location.replace("dashboard.html");
   alert("logged Out");
 }
 //
 // function saveStoreData() {
 //
 // }
+
+  var i = 0;
+  function move() {
+    if (i == 0) {
+      i = 1;
+      var elem = document.getElementById("myBar");
+      var width = 1;
+      var id = setInterval(frame, 40);
+      function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+          i = 0;
+        } else {
+          width++;
+          elem.style.width = width + "%";
+        }
+      }
+    }
+  }
